a367c78422a17d7f7fa3af692bc0a782b966f724
moskvichev <moskvichev@keeneye.pro>
2025-12-21 01:24:19 +0400

    kal 2

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/data-storage-calculator/',
   build: {
     outDir: 'dist',
     sourcemap: false,
